f99bac5a4856528f3580e5e46c2c68c1afbc6b46
Dario Costa <dario.costa@castrosua.com>
2025-12-23 12:33:41 +0100

    Optimize ChangeDelta.applyTo using batched list operations

--- java-diff-utils/src/main/java/com/github/difflib/patch/ChangeDelta.java
+++ java-diff-utils/src/main/java/com/github/difflib/patch/ChangeDelta.java
@@ -42,14 +42,9 @@ public final class ChangeDelta<T> extends AbstractDelta<T> {
 		protected void applyTo(List<T> target) throws PatchFailedException {
 				int position = getSource().getPosition();
 				int size = getSource().size();
-				for (int i = 0; i < size; i++) {
-						target.remove(position);
-				}
-				int i = 0;
-				for (T line : getTarget().getLines()) {
-						target.add(position + i, line);
-						i++;
-				}
+
+				target.subList(position, position + size).clear();
+				target.addAll(position, getTarget().getLines());
 		}
 
 		@Override
